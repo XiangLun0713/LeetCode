a942cd5dd6d0fc405a32c1dbfbc6c4052501dd97
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-01-30 20:57:47 +0800

    Update 0189 Rotate Array.java

--- Solution/Medium/0189 Rotate Array.java
+++ Solution/Medium/0189 Rotate Array.java
@@ -1,15 +1,11 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int[] buffer = new int[k];
-        if (k <= nums.length) {
-            System.arraycopy(nums, nums.length - k, buffer, 0, k);
-            System.arraycopy(nums, 0, nums, k, nums.length - k);
-            System.arraycopy(buffer, 0, nums, 0, k);
-        } else {
+        if (k > nums.length) {
             k %= nums.length;
-            System.arraycopy(nums, nums.length - k, buffer, 0, k);
-            System.arraycopy(nums, 0, nums, k, nums.length - k);
-            System.arraycopy(buffer, 0, nums, 0, k);
         }
+        System.arraycopy(nums, nums.length - k, buffer, 0, k);
+        System.arraycopy(nums, 0, nums, k, nums.length - k);
+        System.arraycopy(buffer, 0, nums, 0, k);
     }
 }
