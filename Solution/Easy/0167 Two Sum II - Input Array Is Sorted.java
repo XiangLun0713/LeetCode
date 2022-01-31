1a5841b309a8adf5358a1205b5a8125968b0ced1
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-01-31 12:29:31 +0800

    Update 0167 Two Sum II - Input Array Is Sorted.java

--- Solution/Easy/0167 Two Sum II - Input Array Is Sorted.java
+++ Solution/Easy/0167 Two Sum II - Input Array Is Sorted.java
@@ -1,9 +1,19 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         for (int i = 0; i < numbers.length - 1; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[i] + numbers[j] == target) {
-                    return new int[]{i + 1, j + 1};
+            int requiredNumber = target - numbers[i];
+
+            // Use binary search to search for the required number
+            int high = numbers.length - 1;
+            int low = i + 1;
+            while (high >= low) {
+                int mid = low + (high - low) / 2;
+                if (numbers[mid] > requiredNumber) {
+                    high = mid - 1;
+                } else if (numbers[mid] < requiredNumber) {
+                    low = mid + 1;
+                } else {
+                    return new int[]{i + 1, mid + 1};
                 }
             }
         }
