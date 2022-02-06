88aea888d8e8647b7dd5728281b1b4f9e4381f6a
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-02-06 11:35:00 +0800

    Update 0542 01 Matrix.java

--- Solution/Medium/0542 01 Matrix.java
+++ Solution/Medium/0542 01 Matrix.java
@@ -1,8 +1,10 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
         Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[i].length; j++) {
+        int row = mat.length;
+        int column = mat[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
                 if (mat[i][j] == 0) {
                     queue.offer(new int[]{i, j});
                 } else {
@@ -10,35 +12,16 @@ class Solution {
                 }
             }
         }
+        int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         while (!queue.isEmpty()) {
             int[] indices = queue.poll();
-            try {
-                if (mat[indices[0] - 1][indices[1]] == -1) {
-                    mat[indices[0] - 1][indices[1]] = mat[indices[0]][indices[1]] + 1;
-                    queue.offer(new int[]{indices[0] - 1, indices[1]});
-                }
-            } catch (ArrayIndexOutOfBoundsException ignored) {
-            }
-            try {
-                if (mat[indices[0] + 1][indices[1]] == -1) {
-                    mat[indices[0] + 1][indices[1]] = mat[indices[0]][indices[1]] + 1;
-                    queue.offer(new int[]{indices[0] + 1, indices[1]});
-                }
-            } catch (ArrayIndexOutOfBoundsException ignored) {
-            }
-            try {
-                if (mat[indices[0]][indices[1] - 1] == -1) {
-                    mat[indices[0]][indices[1] - 1] = mat[indices[0]][indices[1]] + 1;
-                    queue.offer(new int[]{indices[0], indices[1] - 1});
-                }
-            } catch (ArrayIndexOutOfBoundsException ignored) {
-            }
-            try {
-                if (mat[indices[0]][indices[1] + 1] == -1) {
-                    mat[indices[0]][indices[1] + 1] = mat[indices[0]][indices[1]] + 1;
-                    queue.offer(new int[]{indices[0], indices[1] + 1});
-                }
-            } catch (ArrayIndexOutOfBoundsException ignored) {
+            for (int[] res : dir) {
+                int currentRow = indices[0] + res[0];
+                int currentColumn = indices[1] + res[1];
+                if (currentColumn < 0 || currentColumn >= column || currentRow < 0 || currentRow >= row || mat[currentRow][currentColumn] != -1)
+                    continue;
+                mat[currentRow][currentColumn] = mat[indices[0]][indices[1]] + 1;
+                queue.offer(new int[]{currentRow, currentColumn});
             }
         }
         return mat;
