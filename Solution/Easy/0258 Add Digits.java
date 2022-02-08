690e7e70c2ebd612aba9c3b42fc2ae97142d3307
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-02-08 12:12:13 +0800

    Update 0258 Add Digits.java

--- Solution/Easy/0258 Add Digits.java
+++ Solution/Easy/0258 Add Digits.java
@@ -1,12 +1,10 @@
 class Solution {
     public int addDigits(int num) {
         while (num / 10 != 0) {
-            String sNum = Integer.toString(num);
-            int sum = 0;
-            for (int i = 0; i < sNum.length(); i++) {
-                sum += Integer.parseInt(String.valueOf(sNum.charAt(i)));
-            }
-            num = sum;
+            int lastTwoDigit = num % 100;
+            num = (num - lastTwoDigit) / 10;
+            int sum = (lastTwoDigit / 10) + (lastTwoDigit % 10);
+            num += sum;
         }
         return num;
     }
