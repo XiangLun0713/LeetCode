9310ae4fbbf05f61cb015f9b236c0767a73cb18e
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-02-13 11:11:16 +0800

    Update 0078 Subsets.java

--- Solution/Medium/0078 Subsets.java
+++ Solution/Medium/0078 Subsets.java
@@ -2,26 +2,17 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         result.add(new ArrayList<>());
-        HashSet<Integer> numbers = new HashSet<>();
-        for (int num : nums) {
-            numbers.add(num);
-        }
-        for (int res : numbers) {
-            dfs(result, numbers, new ArrayList<>(), res);
-        }
+        Arrays.sort(nums);
+        dfs(result, new ArrayList<>(), nums, 0);
         return result;
     }
 
-    private void dfs(List<List<Integer>> result, HashSet<Integer> numbers, List<Integer> path, int currentNumber) {
-        HashSet<Integer> newNumbersSet = new HashSet<>(numbers);
-        List<Integer> newPath = new ArrayList<>(path);
-        newNumbersSet.remove(currentNumber);
-        newPath.add(currentNumber);
-        result.add(newPath);
-        for (int res : newNumbersSet) {
-            if (res > currentNumber) {
-                dfs(result, newNumbersSet, newPath, res);
-            }
+    private void dfs(List<List<Integer>> result, List<Integer> path, int[] nums, int start) {
+        for (int i = start; i < nums.length; i++) {
+            List<Integer> tempPath = new ArrayList<>(path);
+            tempPath.add(nums[i]);
+            result.add(tempPath);
+            dfs(result, tempPath, nums, i + 1);
         }
     }
 }
