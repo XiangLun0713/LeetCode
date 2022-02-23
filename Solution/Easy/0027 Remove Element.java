dd93befbe58b5cba67d572e99b386f1e59a973d0
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-02-23 16:13:31 +0800

    Create 0027 Remove Element.java

---
+++ Solution/Easy/0027 Remove Element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        for (int i = 0, j = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+                count++;
+            }
+        }
+        return count;
+    }
+}
