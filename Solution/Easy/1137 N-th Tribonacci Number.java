b0faaebc554f8dd7d281809a759f332babdb7074
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-02-24 19:30:46 +0800

    Create 1137 N-th Tribonacci Number.java

---
+++ Solution/Easy/1137 N-th Tribonacci Number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n == 0) return 0;
+        if(n <= 2) return 1;
+        int[] cache = new int[n + 1];
+        cache[0] = 0;
+        cache[1] = 1;
+        cache[2] = 1;        
+        for (int i = 3; i < n + 1; i++) {
+            cache[i] = cache[i - 3] + cache[i - 2] + cache[i - 1];
+        }
+        return cache[n];
+    }
+}
