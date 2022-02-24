50cd15666476a6085bde2eea19e9068cad22c4c8
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-02-24 18:40:35 +0800

    Create 0148 Sort List.java

---
+++ Solution/Medium/0148 Sort List.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode midNode = getMid(head);
+        ListNode rightNode = midNode.next;
+        midNode.next = null;
+
+        ListNode sortedLeft = sortList(rightNode);
+        ListNode sortedRight = sortList(head);
+
+        return mergeNode(sortedLeft, sortedRight);
+    }
+
+    private ListNode mergeNode(ListNode leftNode, ListNode rightNode) {
+        ListNode result = new ListNode(-1);
+        ListNode leftPointer = leftNode;
+        ListNode rightPointer = rightNode;
+        ListNode resultPointer = result;
+
+        while (leftPointer != null && rightPointer != null) {
+            int left = leftPointer.val;
+            int right = rightPointer.val;
+            if (left < right) {
+                resultPointer.next = new ListNode(left);
+                leftPointer = leftPointer.next;
+            } else {
+                resultPointer.next = new ListNode(right);
+                rightPointer = rightPointer.next;
+            }
+            resultPointer = resultPointer.next;
+        }
+        if (rightPointer != null) {
+            resultPointer.next = rightPointer;
+        } else if (leftPointer != null) {
+            resultPointer.next = leftPointer;
+        }
+        return result.next;
+    }
+
+    private ListNode getMid(ListNode node) {
+        int count = 0;
+        ListNode pointer = node;
+        List<ListNode> list = new ArrayList<>();
+        while (pointer != null) {
+            list.add(pointer);
+            pointer = pointer.next;
+            count++;
+        }
+        return list.get((count / 2) - 1);
+    }
+}
