c7db83a10555e8eb46c71bf8ca8f2de707706640
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-02-25 14:32:00 +0800

    Update 0070 Climbing Stairs.java

--- Solution/Easy/0070 Climbing Stairs.java
+++ Solution/Easy/0070 Climbing Stairs.java
@@ -1,16 +1,13 @@
 class Solution {
-    public int climbStairs(int n) {
-        Integer[] memoize = new Integer[n + 1];
-        memoize[n] = 1;
-        memoize[n - 1] = 1;
-        findNumberOfWays(n, memoize);
-        return memoize[0];
-    }
-
-    private void findNumberOfWays(int n, Integer[] memoize) {
-        if (n - 2 >= 0) {
-            memoize[n - 2] = memoize[n] + memoize[n - 1];
-            findNumberOfWays(n - 1, memoize);
+    public  int climbStairs(int n) {
+        if (n <= 2) return n;
+        int[] cache = new int[2];
+        cache[0] = 1;
+        cache[1] = 2;
+        int i = 0;
+        for (; i < n - 2; i++) {
+            cache[i % 2] = cache[0] + cache[1];
         }
+        return cache[(i - 1) % 2];
     }
 }
