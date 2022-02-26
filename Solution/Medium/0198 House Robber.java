51b1a7a795f6670d861fe5fb0a5e854470446ae1
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-02-26 13:37:03 +0800

    Update 0198 House Robber.java

--- Solution/Medium/0198 House Robber.java
+++ Solution/Medium/0198 House Robber.java
@@ -1,11 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
-        int rob1 = 0, rob2 = 0;
-        for (int num : nums) {
-            int temp = Math.max(num + rob1, rob2);
-            rob1 = rob2;
-            rob2 = temp;
+        if(nums.length == 1) return nums[0];
+        
+        int[] arr = new int[nums.length + 2];
+        System.arraycopy(nums, 0, arr, 2, nums.length);
+        
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] = Math.max(arr[i] + arr[i - 2], arr[i - 1]);
         }
-        return rob2;
+        
+        return arr[arr.length - 1];
     }
 }
