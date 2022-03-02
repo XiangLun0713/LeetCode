6aeba5b7f107c883623376abb08b0ce2688b3cb5
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-03-02 10:36:18 +0800

    Create 0392 Is Subsequence.java

---
+++ Solution/Easy/0392 Is Subsequence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() > t.length()) return false;
+        if (s.length() == 0 && t.length() == 0) return true;
+        if (s.length() == 0 || t.length() == 0) return s.length() == 0;
+        
+        int j = 0;
+        for (int i = 0; i < t.length(); i++) {
+            if (s.charAt(j) == t.charAt(i)) j++;
+            if (j == s.length()) return true;
+        }
+        return false;
+    }
+}
