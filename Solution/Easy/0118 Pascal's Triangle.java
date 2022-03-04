1a644bc64775df9528bf7466c292982fd9cbd029
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-03-04 13:31:16 +0800

    Update 0118 Pascal's Triangle.java

--- Solution/Easy/0118 Pascal's Triangle.java
+++ Solution/Easy/0118 Pascal's Triangle.java
@@ -1,17 +1,25 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> result = new ArrayList<>();
-        int[] preArr = new int[0];
-        for (int i = 0; i < numRows; i++) {
-            int[] arr = new int[i + 1];
-            arr[0] = 1;
-            arr[i] = 1;
-            for (int j = 1; j < i; j++) {
-                arr[j] = preArr[j - 1] + preArr[j];
-            }
+        int[] preArr = {1};
+        result.add(Arrays.stream(preArr).boxed().toList());
+        for (int i = 1; i < numRows; i++) {
+            int[] arr1 = new int[i + 1];
+            System.arraycopy(preArr, 0, arr1, 0, preArr.length);
+            int[] arr2 = new int[i + 1];
+            System.arraycopy(preArr, 0, arr2, 1, preArr.length);
+            int[] arr = addArrays(arr1, arr2);
             preArr = arr;
-            result.add(new ArrayList<>(Arrays.stream(arr).boxed().toList()));
+            result.add(Arrays.stream(arr).boxed().toList());
         }
         return result;
+    }
+
+    private int[] addArrays(int[] a, int[] b) {
+        int[] arr = new int[a.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = a[i] + b[i];
+        }
+        return arr;
     }
 }
