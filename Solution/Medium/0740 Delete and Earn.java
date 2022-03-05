a479327e5e6a5437a2e5f859c41e7bee235e90a1
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-03-05 18:22:34 +0800

    Update 0740 Delete and Earn.java

--- Solution/Medium/0740 Delete and Earn.java
+++ Solution/Medium/0740 Delete and Earn.java
@@ -1,27 +1,28 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
-        if (nums.length == 1) return nums[0];
-
-        int[] arr = new int[10000];
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int res : nums) {
-            arr[res]++;
-            if (!list.contains(res)) {
-                list.add(res);
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int num : nums) {
+            if (!hashMap.containsKey(num)) {
+                hashMap.put(num, 1);
+            } else {
+                hashMap.put(num, hashMap.get(num) + 1);
             }
         }
+        ArrayList<Integer> list = new ArrayList<>(hashMap.keySet());
         Collections.sort(list);
 
-        int earn1 = 0, earn2 = list.get(0) * arr[list.get(0)];
-        for (int i = 1; i < list.size(); i++) {
-            int temp;
-            if (list.get(i - 1) == list.get(i) - 1) {
-                temp = Math.max(earn1 + list.get(i) * arr[list.get(i)], earn2);
+        int earn1 = 0, earn2 = 0, prevNum = -1;
+        for (int key : list) {
+            int currVal = key * hashMap.get(key);
+            if (prevNum != key - 1) {
+                earn1 = earn2;
+                earn2 += currVal;
             } else {
-                temp = earn2 + list.get(i) * arr[list.get(i)];
+                int temp = Math.max(earn1 + currVal, earn2);
+                earn1 = earn2;
+                earn2 = temp;
             }
-            earn1 = earn2;
-            earn2 = temp;
+            prevNum = key;
         }
         return earn2;
     }
