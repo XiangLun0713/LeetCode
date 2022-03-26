33a0eb9ce1a238218765bd854b0ce7eaffa44b93
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-03-26 09:06:02 +0800

    Update 0704 Binary Search.java

--- Solution/Easy/0704 Binary Search.java
+++ Solution/Easy/0704 Binary Search.java
@@ -1,8 +1,15 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                return i;
+        int high = nums.length - 1;
+        int low = 0;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > target) {
+                high = mid - 1;
+            }else if (nums[mid] < target) {
+                low = mid + 1;
+            }else {
+                return mid; 
             }
         }
         return -1;
