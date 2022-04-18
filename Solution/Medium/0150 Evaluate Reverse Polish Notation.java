b2eca07bed5600c838d9c2bf5137f53d8d745827
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-04-18 13:16:19 +0800

    Update 0150 Evaluate Reverse Polish Notation.java

--- Solution/Medium/0150 Evaluate Reverse Polish Notation.java
+++ Solution/Medium/0150 Evaluate Reverse Polish Notation.java
@@ -2,20 +2,31 @@ class Solution {
     public int evalRPN(String[] tokens) {
         Deque<Integer> deque = new ArrayDeque<>();
         for (String res : tokens) {
-            if (res.equals("+")) {
-                deque.push(deque.pop() + deque.pop());
-            } else if(res.equals("*")) {
-                deque.push(deque.pop() * deque.pop());
-            } else if(res.equals("-")) {
-                int temp = deque.pop();
-                deque.push(deque.pop() - temp);
-            } else if(res.equals("/")) {
-                int temp = deque.pop();
-                deque.push(deque.pop() / temp);
+            if (res.equals("+") || res.equals("-") || res.equals("*") || res.equals("/")) {
+                deque.push(compute(deque.pop(), deque.pop(), res));
             } else {
                 deque.push(Integer.parseInt(res));
             }
         }
         return deque.pop();
+    }
+    private int compute(int operand1, int operand2, String operator) {
+        switch(operator) {
+            case "+" -> {
+                return operand2 + operand1;
+            }
+            case "-" -> {
+                return operand2 - operand1;
+            }
+            case "*" -> {
+                return operand2 * operand1;
+            }
+            case "/" -> {
+                return operand2 / operand1;
+            }
+            default -> {
+                return 0;
+            }
+        }
     }
 }
