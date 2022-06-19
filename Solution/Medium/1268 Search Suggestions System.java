56a01e891957e115a6d98ed3d8973c92b6184c14
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-06-19 09:51:37 +0800

    Create 1268 Search Suggestions System.java

---
+++ Solution/Medium/1268 Search Suggestions System.java
@@ -0,0 +1,57 @@
+class Trie {
+
+    private Node root;
+
+    public Trie() {
+        root = new Node('\0');
+    }
+
+    public void insert(String word) {
+        Node curr = root;
+        for (char c : word.toCharArray()) {
+            if (curr.children[c - 'a'] == null) {
+                curr.children[c - 'a'] = new Node(c);
+            }
+            curr = curr.children[c - 'a'];
+        }
+        curr.isWord = true;
+    }
+
+    public boolean search(String word) {
+        Node curr = root;
+        for (char c : word.toCharArray()) {
+            if (curr.children[c - 'a'] == null) return false;
+            curr = curr.children[c - 'a'];
+        }
+        return curr.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        Node curr = root;
+        for (char c : prefix.toCharArray()) {
+            if (curr.children[c - 'a'] == null) return false;
+            curr = curr.children[c - 'a'];
+        }
+        return true;
+    }
+
+    static class Node {
+        public char c;
+        public boolean isWord;
+        public Node[] children;
+
+        public Node(char c) {
+            this.c = c;
+            isWord = false;
+            children = new Node[26];
+        }
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
