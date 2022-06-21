181af21230d45789cd5eec343b974b7c82ecef09
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-06-21 10:00:43 +0800

    Create Day 21 Furthest Building You Can Reach.java

---
+++ June LeetCoding Challenge 2022/Day 21 Furthest Building You Can Reach.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i = 1; i < heights.length; i++) {
+            int diff = heights[i] - heights[i - 1];
+            if (diff > 0) {
+                queue.offer(diff);
+                if (queue.size() > ladders) {
+                    bricks -= queue.poll();
+                    if (bricks < 0) {
+                        return i - 1;
+                    }
+                }
+            }
+        }  
+        return heights.length - 1;
+    }
+}
