533ed80ae70110f9c980b0cd42184217b24bce62
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-06-25 23:55:02 +0800

    Create Day 25 Non-decreasing Array.java

---
+++ June LeetCoding Challenge 2022/Day 25 Non-decreasing Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int count = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                if(count++ > 0 || ((i > 1 && i < nums.length - 1 && nums[i - 2] > nums[i] && nums[i + 1] < nums[i - 1]))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
