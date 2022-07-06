790512e5f52a9b0c0ea1978f2bab193efcca38c8
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-07-06 10:19:19 +0800

    Update 0509 Fibonacci Number.java

--- Solution/Easy/0509 Fibonacci Number.java
+++ Solution/Easy/0509 Fibonacci Number.java
@@ -1,13 +1,12 @@
 class Solution {
     public int fib(int n) {
-        if (n == 0) return 0;
-        if (n <= 2) return 1;
-        int[] cache = new int[n];
-        cache[0] = 1;
+        int[] cache = new int[2];
         cache[1] = 1;
-        for(int i = 2; i < n; i++) {
-            cache[i] = cache[i - 1] + cache[i - 2];
+        
+        for (int i = 2; i <= n; i++) {
+            cache[i % 2] = cache[0] + cache[1];
         }
-        return cache[n - 1];
+   
+        return cache[n % 2];
     }
 }
