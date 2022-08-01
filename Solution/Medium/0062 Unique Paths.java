84c005a18621afefb8519d805d333057318df8fc
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-08-01 08:45:30 +0800

    Update 0062 Unique Paths.java

--- Solution/Medium/0062 Unique Paths.java
+++ Solution/Medium/0062 Unique Paths.java
@@ -1,17 +1,14 @@
 // Using Dynamic Programming method
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] cache = new int[m][n];
-        Arrays.fill(cache[m - 1], 1);
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < n; i++) dp[m - 1][i] = 1;
+        for (int i = 0; i < m; i++) dp[i][n - 1] = 1;
         for (int i = m - 2; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                if (j == n - 1) {
-                    cache[i][j] = 1;
-                } else {
-                    cache[i][j] = cache[i + 1][j] + cache[i][j + 1];
-                }
+            for (int j = n - 2; j >= 0; j--) {
+                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
             }
         }
-        return cache[0][0];
+        return dp[0][0];
     }
 }
