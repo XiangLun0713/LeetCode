9dce94ce5fd119ed24b5da7a36129bf1de04aa7b
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-08-16 13:19:19 +0800

    Create Day 16 First Unique Character in a String.java

---
+++ August LeetCoding Challenge 2022/Day 16 First Unique Character in a String.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            hashMap.put(c, hashMap.getOrDefault(c, 0) + 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (hashMap.get(c) == 1) return i;
+        }
+        return -1;
+    }
+}
