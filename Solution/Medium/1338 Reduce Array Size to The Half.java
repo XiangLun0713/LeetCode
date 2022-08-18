5dd742debc962a68bde03e9a148ba90ae5dbdc70
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2022-08-18 15:13:27 +0800

    Create 1338 Reduce Array Size to The Half.java

---
+++ Solution/Medium/1338 Reduce Array Size to The Half.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minSetSize(int[] arr) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int num : arr) {
+            hashMap.put(num, hashMap.getOrDefault(num, 0) + 1);
+        }
+        
+        int size = arr.length;
+        int halfSize = arr.length / 2;
+        int result = 0;
+        
+        List<Integer> freqList = new ArrayList<>(hashMap.values());
+        Collections.sort(freqList, Collections.reverseOrder());
+        
+        for (int freq : freqList) {
+            if (size <= halfSize) break;
+            size -= freq;
+            result++;
+        }
+        
+        return result;
+    }
+}
