ec6f52856bb0ee9464c6d67f5c0d4e6a5aab6973
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2023-02-18 12:27:14 +0800

    Create Day 18 Invert Binary Tree.java

---
+++ February LeetCoding Challenge 2023/Day 18 Invert Binary Tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        traverse(root);
+        return root;
+    }
+    private void traverse(TreeNode root) {
+        if (root == null) return;
+        TreeNode left = root.left;
+        root.left = root.right;
+        root.right = left;
+        traverse(root.left);
+        traverse(root.right);
+    }
+}
