f650b84f88eb642765f87ad7ae37b3c2fc458815
CHOO XIANG LUN <93761074+XiangLun0713@users.noreply.github.com>
2023-03-01 19:54:05 +0800

    Create 0912 Sort an Array.java

---
+++ Solution/Medium/0912 Sort an Array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int num : nums) pq.offer(num);
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = pq.poll();
+        }
+        return nums;
+    }
+}
