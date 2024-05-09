7acaf6be9a214ff76da83069944b2e40db94bead
CHOO XIANG LUN <xlchoo25@gmail.com>
2024-05-09 13:47:01 +0800

    Update 3075 Maximize Happiness of Selected Children.kt

--- Solution/Medium/3075 Maximize Happiness of Selected Children.kt
+++ Solution/Medium/3075 Maximize Happiness of Selected Children.kt
@@ -1,8 +1,6 @@
-class Solution {
-    fun maximumHappinessSum(happiness: IntArray, k: Int): Long {
-        var result = 0
-        return happiness.map { it.toLong() }.sortedDescending().take(k).sumOf { value ->
-            max(0L, value - result++)
-        }
+fun maximumHappinessSum(happiness: IntArray, k: Int): Long {
+    var i = 0
+    return happiness.sortedDescending().take(k).sumOf { value ->
+        max(0L, value.toLong() - i++)
     }
 }
